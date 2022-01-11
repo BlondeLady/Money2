eac091b0bd8232f6492209a5d3470f2a36176872
NIKA <NIKA@WIN-DU6B2AH9M6B>
2022-01-11 23:16:08 +0200

    Добавьте файлы проекта.

---
+++ Money.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money2
+{
+    class Money
+    {
+        private int _grivna;
+        private int _kopiyka;
+        static double _dollarCost;
+        public Money() { }
+        public Money(int grivna, int kopiyka)
+        {
+            Grivna = grivna;
+            Kopiyka = kopiyka;
+        }
+        public int Grivna
+        {
+            get => _grivna;
+            set => _grivna = value != 0 ? value : 0;
+        }
+        public int Kopiyka
+        {
+            get => _kopiyka;
+            set => _kopiyka = value != 0 ? value : 0;
+        }
+        static Money()
+        {
+            _dollarCost = 28.9;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0: return _grivna;
+                    case 1: return _kopiyka;
+
+                    default: throw new Exception("неверный индекс");
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        _grivna = value;
+                        break;
+                    case 1:
+                        _kopiyka = value;
+                        break;
+                }
+            }
+
+        }
+
+        public static Money operator +(Money mon1, Money mon2)
+        {
+            int b = mon1._kopiyka + mon2._kopiyka;
+            int a = mon1._grivna + mon2._grivna;
+            if (mon1._kopiyka + mon2._kopiyka >= 100)
+            {
+                a = mon1._grivna + mon2._grivna + 1;
+                b = mon1._kopiyka + mon2._kopiyka - 100;
+            }
+            return new Money(a, b, _dollarCost);
+        }
+        public static Money operator -(Money mon1, Money mon2)
+        {
+            return new Money(mon1._grivna - mon2._grivna, mon1._kopiyka - mon2._kopiyka, _dollarCost);
+        }
+        public override string ToString() => $"{Grivna} {Kopiyka} {_dollarCost}";
+        public double ToDollar()
+        {
+            double dollar = (_grivna + _kopiyka / 100) / _dollarCost;
+            return dollar;
+        }
+        public void FromDollar(double dollar)
+        {
+            double total_ = _grivna * _dollarCost;
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money mon1 = new Money();
+            mon1.Grivna = 15;
+            mon1.Kopiyka = 78;
+            mon1[1] = 90;
+            Money mon2 = new Money(10, 23);
+
+            Money total = mon1 + mon2;
+            Console.WriteLine($"Money mon1:{mon1}");
+            Console.WriteLine($"Money mon2:{mon2}");
+            Console.WriteLine($"Total money:{total}");
+
+            
+            Console.WriteLine($"Total in dollar:{total.ToDollar()}");
+            total.FromDollar(100);
+            Console.WriteLine($"Total money:{total}");
+
+        }
+    }
+}
